0cd037bf075e8d0c4210ed167d50bdd72af66b81
juandaantoniusapakpahan <jujupapa06@gmail.com>
2023-04-22 22:13:26 +0700

    add default select

--- goroutine_test.go
+++ goroutine_test.go
@@ -89,3 +89,29 @@ func TestSelectChannel(t *testing.T) {
 		}
 	}
 }
+
+func TestDefaultSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+
+	go ChannelAsParam(channel1)
+	go ChannelAsParam(channel2)
+	counter := 0
+
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Channel 1: ", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Channel 2: ", data)
+			counter++
+
+		default:
+			fmt.Println("Menunggu hal yang membosankan...")
+		}
+		if counter == 2 {
+			break
+		}
+	}
+}
